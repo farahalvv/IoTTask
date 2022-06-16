33befe8a85f2ee0f0605e34623e3f1979cfdec0e
farahalvv <100007532+farahalvv@users.noreply.github.com>
2022-06-16 08:46:43 +0700

    Add files via upload

---
+++ speaker.ino
@@ -0,0 +1,9 @@
+void M5.Speaker.tone(uint16_t freq, uint32_t duration);
+Description：Play audio at a specified frequency and support
+setting the playing time.
+parameter：
+uint16_t freq: Set the frequency of playing
+sound uint32_t duration:
+return:none
+eg：
+M5.Speaker.tone(440, 1000);
